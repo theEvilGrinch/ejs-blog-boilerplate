96c642e7a69bdd26de687af57fdadf833a83ec2a
Andrew Dmitriev <andrew.dmitriev.22@gmail.com>
2025-06-20 15:25:52 +0300

    feat(theme): persist selection across sessions

--- src/scripts/set-color-theme.mjs
+++ src/scripts/set-color-theme.mjs
@@ -1,13 +1,18 @@
 function setColorTheme() {
-  const isBrowserThemeDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
   const colorThemeButton = document.querySelector('[data-toggle-theme]');
-  document.body.classList.add(isBrowserThemeDark ? 'dark-mode' : 'light-mode');
+
+  const savedTheme = localStorage.getItem('theme');
+  const isBrowserThemeDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const initialTheme = savedTheme || (isBrowserThemeDark ? 'dark-mode' : 'light-mode');
+
+  document.body.classList.add(initialTheme);
 
   colorThemeButton.addEventListener('click', () => {
     const isCurrentThemeDark = document.body.classList.contains('dark-mode');
-    const setDarkTheme = () => document.body.classList.replace('light-mode', 'dark-mode');
-    const setLightTheme = () => document.body.classList.replace('dark-mode', 'light-mode');
-    isCurrentThemeDark ? setLightTheme() : setDarkTheme();
+    const newTheme = isCurrentThemeDark ? 'light-mode' : 'dark-mode';
+
+    document.body.classList.replace(isCurrentThemeDark ? 'dark-mode' : 'light-mode', newTheme);
+    localStorage.setItem('theme', newTheme);
   });
 }
 
